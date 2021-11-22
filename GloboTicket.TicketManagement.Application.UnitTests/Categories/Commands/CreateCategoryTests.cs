bfc00d017d1844aec0be6e80f92c8adb0ee6a909
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2021-11-22 17:29:15 -0600

    Implemented Unit Tests

---
+++ GloboTicket.TicketManagement.Application.UnitTests/Categories/Commands/CreateCategoryTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using GloboTicket.TicketManagement.Application.Contracts;
+using GloboTicket.TicketManagement.Application.Features.Categories.Commands.CreateCategory;
+using GloboTicket.TicketManagement.Application.Profiles;
+using GloboTicket.TicketManagement.Application.UnitTests.Mocks;
+using GloboTicket.TicketManagement.Domain.Entities;
+using Shouldly;
+using Xunit;
+
+namespace GloboTicket.TicketManagement.Application.UnitTests.Categories.Commands
+{
+   public class CreateCategoryTests
+   {
+      private readonly IMapper mapper;
+      private readonly Mock<IAsyncRepository<Category>> mockCategoryRepo;
+
+      public CreateCategoryTests()
+      {
+         mockCategoryRepo = RepositoryMocks.GetCategoryRepository();
+         var configProvider = new MapperConfiguration(cfg =>
+         {
+            cfg.AddProfile<MappingProfile>();
+         });
+
+         mapper = configProvider.CreateMapper();
+      }
+
+      [Fact]
+      public async Task Handle_ValidCategory_AddedToCategoriesRepo()
+      {
+         var handler = new CreateCategoryHandler(mockCategoryRepo.Object, mapper);
+         var result = await handler.Handle(new CreateCategoryCommand { Name = "Test" }, CancellationToken.None);
+
+         var allCategories = await mockCategoryRepo.Object.ListAllAsync();
+
+         allCategories.Count.ShouldBe(5);
+      }
+   }
+}
---
+++ GloboTicket.TicketManagement.Application.UnitTests/Categories/Queries/GetCategoriesListQueryHandlerTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using GloboTicket.TicketManagement.Application.Contracts;
+using GloboTicket.TicketManagement.Application.Features.Categories.Queries.GetCategoriesList;
+using GloboTicket.TicketManagement.Application.Profiles;
+using GloboTicket.TicketManagement.Application.UnitTests.Mocks;
+using GloboTicket.TicketManagement.Domain.Entities;
+using Shouldly;
+using Xunit;
+
+namespace GloboTicket.TicketManagement.Application.UnitTests
+{
+   public class GetCategoriesListQueryHandlerTests
+   {
+      private readonly IMapper mapper;
+      private readonly Mock<IAsyncRepository<Category>> mockCategoryRepo;
+
+      public GetCategoriesListQueryHandlerTests()
+      {
+         mockCategoryRepo = RepositoryMocks.GetCategoryRepository();
+         var configProvider = new MapperConfiguration(cfg =>
+         {
+            cfg.AddProfile<MappingProfile>();
+         });
+
+         mapper = configProvider.CreateMapper();
+      }
+
+      [Fact]
+      public async Task GetCategoriesListTest()
+      {
+         var handler = new GetCategoriesListQueryHandler(mapper,mockCategoryRepo.Object);
+         var result = await handler.Handle(new GetCategoriesListQuery(), CancellationToken.None);
+         result.ShouldBeOfType<List<CategoryListVm>>();
+         result.Count.ShouldBe(4);
+      }
+   }
+}
