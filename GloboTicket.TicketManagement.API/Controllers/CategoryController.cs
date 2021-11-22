744bce5b7e2f60a5ec6b68c69450d1291ecc712a
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2021-11-22 16:12:37 -0600

    Implemented Category Controller

---
+++ GloboTicket.TicketManagement.API/Controllers/CategoryController.cs
@@ -0,0 +1,44 @@
+﻿using GloboTicket.TicketManagement.Application.Features.Categories.Commands.CreateCategory;
+using GloboTicket.TicketManagement.Application.Features.Categories.Queries.GetCategoriesList;
+using GloboTicket.TicketManagement.Application.Features.Categories.Queries.GetCategoriesListWithEvents;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GloboTicket.TicketManagement.API.Controllers
+{
+   [Route("api/[Controller]")]
+   [ApiController]
+   public class CategoryController : Controller
+   {
+      private readonly IMediator mediator;
+
+      public CategoryController(IMediator mediator)
+      {
+         this.mediator = mediator;
+      }
+
+      [HttpGet("All", Name = "GetAllCategories")]
+      [ProducesResponseType(StatusCodes.Status200OK)]
+      public async Task<ActionResult<List<CategoryListVm>>> GetAllCategories()
+      {
+         var dtos = await mediator.Send(new GetCategoriesListQuery());
+         return Ok(dtos);
+      }
+
+      [HttpGet("AllWithEvents", Name = "GetCategoriesWithEvents")]
+      [ProducesDefaultResponseType]
+      [ProducesResponseType(StatusCodes.Status200OK)]
+      public async Task<ActionResult<List<CategoryEventListVm>>> GetCategoriesWithEvents(bool includeHistory)
+      {
+         var dtos = await mediator.Send(new GetCategoriesListWithEventsQuery());
+         return Ok(dtos);
+      }
+
+      [HttpPost(Name ="AddCategory")]
+      public async Task<ActionResult<CreateCategoryCommandResponse>> Create([FromBody] CreateCategoryCommand createCategoryCommand)
+      {
+         var response = await mediator.Send(createCategoryCommand);
+         return Ok(response);
+      }
+   }
+}
